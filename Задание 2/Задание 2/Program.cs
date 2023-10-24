edc378889894651df71f21c3671fe9f9e83e8dd3
IMAnastasi <147606295+IMAnastasi@users.noreply.github.com>
2023-10-24 10:15:41 +0300

    laba

---
+++ Задание 2/Задание 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x1 = ");
+            float x1 = float.Parse(Console.ReadLine());
+            Console.Write("y1 = ");
+            float y1 = float.Parse(Console.ReadLine());
+            Console.Write("x2 = ");
+            float x2 = float.Parse(Console.ReadLine());
+            Console.Write("y2 = ");
+            float y2 = float.Parse(Console.ReadLine());
+            double ab =Math.Abs(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("Расстояние между точками равно + {0}", ab);
+        }
+    }
+}
---
+++ Задание 3/Задание 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное двухзначное число ");
+            int a = int.Parse(Console.ReadLine());
+            int a1 = a % 10;
+            int a2 = (a - a1) / 10;
+            Console.WriteLine("Минимальная цифра  {0}", Math.Min(a1,a2));
+        }
+    }
+}
